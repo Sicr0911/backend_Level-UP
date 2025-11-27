9b23d3ad2345019408998b8113e7ff409eaf641d
Sicr0911 <seb.catalanr@duocuc.cl>
2025-11-27 00:49:30 -0300

    Avance

--- src/main/java/com/ecomerket/services/users/UserServiceImpl.java
+++ src/main/java/com/ecomerket/services/users/UserServiceImpl.java
@@ -7,10 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Optional;
-import java.util.Set;
+
+import java.util.*;
 
 @Service
 public class UserServiceImpl implements UserService {
